93d13505d2cdcc9d24587c60eb62f16a32f470ca
Grumel4 <alexeyleonov231@gmail.com>
2024-04-11 19:59:32 +0300

    'version_1'

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+Public class Program
+{
+    public static void Main(string[] args)
+    {
+        //Ввод строк.
+        string[] initialstrings = GetInitialStrings();
+
+        //Формирование нового массива
+        string[] strings = new string[initialstrings.Length];
+
+        //Вывод результата
+        Console.WriteLine("Новый массив:");
+        foreach (string str in shortStrings)
+        {
+            Console.WriteLine(str);
+        }
+    }
+    private static string[] GetInitialStrings();
+    {
+        //ввод с клавиатуры
+        Console.WriteLine("Введите значения через запятую:");
+        string input = Console.ReadLine();
+        string[] strings = input.Split(',');
+
+        return strings;
+    }
+private static string[] GetShortStrings(string[] initialStrings)
+    {
+      int count = 0;
+      for (int i = 0; i < initialStrings.Length; i++) 
+      {
+        if (initialStrings[i].Length <= 3)
+        {
+          count++;
+        }
+      }
+
+        string[] shortStrings = new string[count];
+        int j = 0;
+        for (int i = 0; i < initialStrings.Length; i++)
+        {
+         if (initialStrings[i].Length <= 3)
+        {
+            shortStrings[j++] = initialStrings[i];
+      }
+    }
+      return shortStrings;
+    }
+}
